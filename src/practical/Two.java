6ab164bd8a26f1bd455124804cf1a452f980f74d
ChuongvTran <chuongv.tran07@gmail.com>
2021-07-05 20:25:54 -0500

    first commit

---
+++ src/practical/Two.java
@@ -0,0 +1,21 @@
+package practical;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Two {
+	
+	public static void main(String[] args) {
+		
+		LinkedList<String> Apple = new LinkedList<String>();
+		
+		Apple.add("A fruit");
+		Apple.add("A tech firm");
+		
+		System.out.println("Apple"+ Apple);
+
+	}
+}
